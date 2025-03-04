fe9b2fbf4eed54af71b7bbcf610f00195de47050
Golluméo <p.mauriello54@gmail.com>
2025-03-05 00:35:22 +0100

    test(OpenBankingProvider): add initial test for OpenBankingProviderContract implementation

---
+++ tests/Feature/OpenBankingProviderTest.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+use function PHPUnit\Framework\assertTrue;
+
+describe('OpenBankingProvider', function () {
+    it('should implement `OpenBankingProviderContract`', function () {
+        assertTrue(true);
+    });
+});
