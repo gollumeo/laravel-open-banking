8656331644307bd8e886c0a669f63c5efeb85cab
Golluméo <p.mauriello54@gmail.com>
2025-03-05 20:00:53 +0100

    test(OpenBankingProvider): refactor test structure and add setup for TinkClient

--- tests/Feature/OpenBankingProviderTest.php
+++ tests/Feature/OpenBankingProviderTest.php
@@ -2,10 +2,9 @@
 
 declare(strict_types=1);
 
-use function PHPUnit\Framework\assertTrue;
-
 describe('OpenBankingProvider', function () {
-    it('should implement `OpenBankingProviderContract`', function () {
-        assertTrue(true);
+    beforeEach(function () {
+        $client = new TinkClient();
     });
+    it('should implement `OpenBankingProviderContract`', function () {});
 });
