bc17506727a0972971eb045847fd3e19f9c29851
Golluméo <p.mauriello54@gmail.com>
2025-03-06 19:35:01 +0100

    test(OpenBankingProvider): enhance tests for provider fetching and contract implementation

--- tests/Feature/OpenBankingProviderTest.php
+++ tests/Feature/OpenBankingProviderTest.php
@@ -2,9 +2,22 @@
 
 declare(strict_types=1);
 
+use Fintrack\LaravelOpenBanking\OpenBankingProviders\TinkProvider;
+use Support\OpenBankingProvidersHelper;
+
 describe('OpenBankingProvider', function () {
-    beforeEach(function () {
-        $client = new TinkClient();
+    it('should be able to fetch providers namespaces', function () {
+        $providers = OpenBankingProvidersHelper::getProviders();
+
+        expect($providers)->toBeArray();
+        foreach ($providers as $provider) {
+            expect($provider)->toBeClass();
+        }
     });
-    it('should implement `OpenBankingProviderContract`', function () {});
+
+    it('should implement `OpenBankingProviderContract`', function () {
+        $provider = new TinkProvider();
+        todo('create contract');
+        expect($provider)->toBeInstanceOf(OpenBankingProviderContract::class);
+    });
 });
