d661937640d14ccf690e10cfd59a68f4097f3139
Golluméo <p.mauriello54@gmail.com>
2025-03-11 16:43:39 +0100

    test(OAuthManager): enhance authentication tests with additional scenarios

--- tests/Unit/OAuthTest.php
+++ tests/Unit/OAuthTest.php
@@ -10,8 +10,26 @@ describe('OAuth', function () {
         expect(OAuthManager::class)->toImplement(AuthContract::class);
     });
 
+    beforeEach(function () {
+        $this->authManager = new OAuthManager();
+    });
+
     it('should return a string when calling `authenticate`', function () {
-        $authManager = new OAuthManager();
-        expect($authManager->authenticate())->toBeString();
+        expect($this->authManager->authenticate())->toBeString();
+    });
+
+    it('should not be authenticated by default', function () {
+        expect($this->authManager->isAuthenticated())->toBeFalse();
+    });
+
+    it('should return true after authentication', function () {
+        $this->authManager->authenticate();
+        expect($this->authManager->isAuthenticated())->toBeTrue();
+    });
+
+    it('should return false after revoking authentication', function () {
+        $this->authManager->authenticate();
+        $this->authManager->revoke();
+        expect($this->authManager->isAuthenticated())->toBeFalse();
     });
 });
