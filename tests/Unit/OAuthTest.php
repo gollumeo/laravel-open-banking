0f710a4bbb14dbb997b65a06acdc972390c363fe
Golluméo <p.mauriello54@gmail.com>
2025-03-11 16:11:21 +0100

    test(OAuthManager): simplify tests by removing redundant method checks

--- tests/Unit/OAuthTest.php
+++ tests/Unit/OAuthTest.php
@@ -7,12 +7,10 @@ use Fintrack\LaravelOpenBanking\Contracts\AuthContract;
 
 describe('OAuth', function () {
     it('should implement `AuthContract`', function () {
-        expect(OAuthManager::class)->toBeClass()
-            ->and(OAuthManager::class)->toImplement(AuthContract::class);
+        expect(OAuthManager::class)->toImplement(AuthContract::class);
     });
 
     it('should return a string when calling `authenticate`', function () {
-        expect(OAuthManager::class)->toHaveMethod('authenticate');
         $authManager = new OAuthManager();
         expect($authManager->authenticate())->toBeString();
     });
