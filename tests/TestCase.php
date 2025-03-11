684ee8700e4a7cdb02bb7a3ca31ba06356468f25
Golluméo <p.mauriello54@gmail.com>
2025-03-11 16:43:29 +0100

    refactor(TestCase): add AuthContract property for improved authentication handling

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,12 +4,15 @@ declare(strict_types=1);
 
 namespace Tests;
 
+use Fintrack\LaravelOpenBanking\Contracts\AuthContract;
 use Fintrack\LaravelOpenBanking\Contracts\OpenBankingProviderContract;
 use PHPUnit\Framework\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
     protected ?string $providerClass = null;
+
+    protected ?AuthContract $authManager = null;
 
     protected ?OpenBankingProviderContract $provider = null;
 }
