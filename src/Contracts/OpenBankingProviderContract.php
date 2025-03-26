4fea2e9368d8a2611e25acfc6609f9c6a539d4bb
Golluméo <p.mauriello54@gmail.com>
2025-03-26 14:52:08 +0100

    feat(OpenBankingProviderContract): add return type annotation for getTransactions method

--- src/Contracts/OpenBankingProviderContract.php
+++ src/Contracts/OpenBankingProviderContract.php
@@ -6,5 +6,8 @@ namespace Fintrack\LaravelOpenBanking\Contracts;
 
 interface OpenBankingProviderContract
 {
+    /**
+     * @return array<string|mixed>
+     */
     public function getTransactions(): array;
 }
