edbf9c870f70bd76c1da235a5b74d9a6c90cd4ac
Golluméo <p.mauriello54@gmail.com>
2025-04-23 01:32:45 +0200

    test(auth): add TDD workflow for ApplicationUrl and enhance GenericAuthentication tests

--- tests/Feature/GenericAuthenticationTest.php
+++ tests/Feature/GenericAuthenticationTest.php
@@ -2,10 +2,37 @@
 
 declare(strict_types=1);
 
+use Fintrack\LaravelOpenBanking\Auth\OpenBankingAuth;
+use Fintrack\LaravelOpenBanking\Infrastructure\Config\ApplicationUrl;
+
 describe('Feature: Generic Authentication', function () {
     it('should have access to a redirect URL when initiating authentication flow', function () {
         // Given
+        $openBankingAuth = new OpenBankingAuth();
         // When
+        $oauthRedirectUrl = $openBankingAuth->init();
         // Then
+        expect($oauthRedirectUrl)->toBeString();
+    });
+
+    it('should return a non-empty redirect URL when initiating authentication flow', function () {
+        // Given
+        $openBankingAuth = new OpenBankingAuth();
+        // When
+        $oauthRedirectUrl = $openBankingAuth->init();
+        // Then
+        expect($oauthRedirectUrl)->not->toBeEmpty();
+    });
+
+    it('should return a valid, app-based redirect URL when initiating authentication flow', function () {
+        $appUrlPrefix = ApplicationUrl::fromConfig();
+        $expectedUrl = $appUrlPrefix.'/oauth-redirect';
+
+        // Given
+        $openBankingAuth = new OpenBankingAuth();
+        // When
+        $oauthRedirectUrl = $openBankingAuth->init();
+        // Then
+        expect($oauthRedirectUrl)->toBe($expectedUrl);
     });
 });
---
+++ tests/Integration/Infrastructure/Config/ApplicationUrlTest.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+use Fintrack\LaravelOpenBanking\Infrastructure\Config\ApplicationUrl;
+
+describe('Unit: Application URL', function () {
+    it('should retrieve the base URL via the configuration key', function () {
+        $baseAppUrl = ApplicationUrl::fromConfig();
+
+        expect($baseAppUrl)->toEqual(config('app.url'));
+    });
+
+    it('should throw an IllegalArgumentException if the app.url key does not exist', function () {
+        config(['app.url' => 123]);
+
+        expect(fn () => ApplicationUrl::fromConfig())->toThrow(InvalidArgumentException::class);
+    });
+});
