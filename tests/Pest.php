34d3664edc23d63658231d5f368ed79d14da6848
Golluméo <p.mauriello54@gmail.com>
2025-04-22 13:15:02 +0200

    refactor(project): delete throwable prototype

--- tests/Pest.php
+++ tests/Pest.php
@@ -13,7 +13,7 @@ declare(strict_types=1);
 |
 */
 
-uses(Tests\TestCase::class)->in(__DIR__);
+// uses(Tests\TestCase::class)->in(__DIR__);
 
 /*
 |--------------------------------------------------------------------------
