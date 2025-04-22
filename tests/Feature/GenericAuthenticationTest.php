e6f6ae952a31fe13f73664b65414c1d4e87a1ced
Golluméo <p.mauriello54@gmail.com>
2025-04-22 17:47:58 +0200

    test(GenericAuthentication): add initial test for authentication flow

---
+++ tests/Feature/GenericAuthenticationTest.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+describe('Feature: Generic Authentication', function () {
+    it('should have access to a redirect URL when initiating authentication flow', function () {
+        // Given
+        // When
+        // Then
+    });
+});
