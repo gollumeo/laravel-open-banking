072e8727ad4ef1bee51d1dea72a7c721964dba38
Golluméo <p.mauriello54@gmail.com>
2025-04-26 12:43:20 +0200

    test(authentication): add initial test for automatic authentication feature

---
+++ tests/Feature/GenericAuthenticationFlowTest.php
@@ -0,0 +1,7 @@
+<?php
+
+declare(strict_types=1);
+
+describe('Feature: Automatic Authentication', function () {
+    it('should authenticate internally when the developer wants to retrieve a resource', function () {});
+});
