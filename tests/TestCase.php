c9021bc97317a989c75e590532b0da2d87e8764c
Golluméo <p.mauriello54@gmail.com>
2025-04-26 12:43:07 +0200

    refactor(tests): simplify TestCase by removing unused OpenBankingServiceProvider

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,14 +4,4 @@ declare(strict_types=1);
 
 namespace Tests;
 
-use Fintrack\LaravelOpenBanking\OpenBankingServiceProvider;
-
-abstract class TestCase extends \Orchestra\Testbench\TestCase
-{
-    protected function getPackageProviders($app): array
-    {
-        return [
-            OpenBankingServiceProvider::class,
-        ];
-    }
-}
+abstract class TestCase extends \Orchestra\Testbench\TestCase {}
